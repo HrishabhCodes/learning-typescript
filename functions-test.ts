08a0b32f25ae358807ebdd4de42ba7f9fb43369b
Hrishabh Jain <hrishabh507@gmail.com>
2023-08-06 04:02:31 +0530

    More on TS functions

--- functions-test.ts
+++ functions-test.ts
@@ -1,3 +1,26 @@
-import add from "./functions";
+import add, { getName } from "./functions";
 
-console.log(add(1, 2));
+console.log(getName({ first: "Hrishabh", last: "Jain" }));
+
+type AddFunction = (a: number, b: number) => number;
+
+const newAddFunc: AddFunction = (a: number, b: number) => a + b * 100;
+console.log(newAddFunc(1, 2));
+
+const showSum = (add: AddFunction): void => {
+  const sum = add(1, 2);
+  console.log(sum);
+};
+
+showSum(add);
+
+// closure
+
+type AdderFunc = (v: number) => number;
+
+const create = (num: number): AdderFunc => {
+  return (val: number): number => val + num;
+};
+
+const temp = create(1);
+console.log(temp(5));
--- functions.ts
+++ functions.ts
@@ -2,4 +2,11 @@ const add = (a: number, b: number): number => {
   return a + b;
 };
 
+const temp = [1, 2, "Hrishabh"].map((v) => v);
+console.log(temp);
+
+export const getName = (user: { first: string; last: string }): string => {
+  return `${user?.first ?? "first"} ${user?.last ?? "last"}`;
+};
+
 export default add;
